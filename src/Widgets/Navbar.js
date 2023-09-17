283ea154a1401fa5516ccd6eaff29054dad85fac
Pradosh <chandpradosh7@gmail.com>
2023-09-17 16:53:19 +0530

    Modified for deploy

--- src/Widgets/Navbar.js
+++ src/Widgets/Navbar.js
@@ -22,7 +22,7 @@ useEffect(() => {
   if (socket.current) {
     socket.current.emit("add-user", user._id);
   }
-}, []); // eslint-disable-line
+}, [socket.current]); // eslint-disable-line
 
 useEffect(() => {
   if (socket.current) {
@@ -31,7 +31,7 @@ useEffect(() => {
       setOnlineUsers(data);
     });
   }
-}, []); // eslint-disable-line
+}, [socket.current]); // eslint-disable-line
 
   const handleLogout = () => {
     logOut();
